023b39d4c5f5d20a7d2978763685effa02b8133a
Iman <ibhlool7@gmail.com>
2019-12-08 22:45:38 +0330

    change output

--- app.py
+++ app.py
@@ -8,14 +8,10 @@ CORS(app)
 
 @app.route('/dth')
 def hello_world():
-    with open("temp.txt", "r+") as file:
+    with open("temp.txt", "r") as file:
         temp = ""
         for line in file.readlines():
             temp = line
             break
         print(temp)
-    with open("temp.txt", "w") as file:
-        file.write(f"${random.randrange(0,100)} , ${random.randrange(0,100)}")
-    temp = temp.replace("$", "")
-    # temp = '{ "data " : +' + '"' + temp + '"' + '}'
     return jsonify(temp)
